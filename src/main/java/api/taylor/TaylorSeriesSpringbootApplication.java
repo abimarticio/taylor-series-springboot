82a2ed9a01d6e1acdcbd833db490021c11ae9cb8
Abigail Marticio <marticiobgl@gmail.com>
2020-11-13 13:09:58 +0800

    chore: add spring boot template

---
+++ src/main/java/api/taylor/TaylorSeriesSpringbootApplication.java
@@ -0,0 +1,13 @@
+package api.taylor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaylorSeriesSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaylorSeriesSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/api/taylor/TaylorSeriesSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package api.taylor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaylorSeriesSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
