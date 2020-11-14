33a69abb91c58cabac1cec5d4c0b95eb56714b92
Abigail Marticio <marticiobgl@gmail.com>
2020-11-14 23:12:07 +0800

    chore: add child class Cosine

--- src/main/java/api/taylor/model/TaylorSeries.java
+++ src/main/java/api/taylor/model/TaylorSeries.java
@@ -1,6 +1,7 @@
 package api.taylor.model;
 
 public class TaylorSeries {
+	
 	static int factorial(int num) {
         if (num == 0) {
             return 1;
@@ -9,4 +10,19 @@ public class TaylorSeries {
             return num * factorial(num - 1);
         }
     }
+}
+
+class Cosine extends TaylorSeries {
+	
+    static double computeSeries(double x, double numTerms) {
+        x  = Math.toRadians(x);
+        double approximation = 0;
+        for (int i = 0; i < numTerms; i++) {
+            double coefficient = Math.pow((-1), i);
+            double numerator = Math.pow(x, (2 * i));
+            double denominator = factorial(2 * i);
+            approximation += (coefficient * (numerator / denominator));
+        }
+        return approximation;
+    } 
 }
