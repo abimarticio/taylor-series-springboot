3a7cf93504600ac650f7ed4381f510e504bb24ff
Abigail Marticio <marticiobgl@gmail.com>
2020-11-14 23:13:10 +0800

    chore: add child class Sine

--- src/main/java/api/taylor/model/TaylorSeries.java
+++ src/main/java/api/taylor/model/TaylorSeries.java
@@ -39,3 +39,17 @@ class Exponential extends TaylorSeries {
         return approximation;
     }
 }
+
+class Sine extends TaylorSeries {
+    static double computeSeries(double x, double numTerms) {
+        x = Math.toRadians(x);
+        double approximation = 0;
+        for (int i = 0; i < numTerms; i++) {
+            double coefficient = Math.pow((-1), i);
+            double numerator = Math.pow(x, (2 * i + 1));
+            double denominator = factorial(2 * i + 1);
+            approximation += (coefficient * (numerator / denominator));
+        }
+        return approximation;
+    }
+}
