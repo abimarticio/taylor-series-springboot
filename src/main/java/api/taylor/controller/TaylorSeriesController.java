6d90729923dfcb75f8b9fabd8e0f9c7e50958357
Abigail Marticio <marticiobgl@gmail.com>
2021-01-05 23:43:28 +0800

    chore: add controller for taylor cosine

--- src/main/java/api/taylor/controller/TaylorSeriesController.java
+++ src/main/java/api/taylor/controller/TaylorSeriesController.java
@@ -1,9 +1,22 @@
 package api.taylor.controller;
 
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import api.taylor.model.TaylorSeries;
 
 
 @RestController
 public class TaylorSeriesController {
-
+	@GetMapping("/taylor-series/cosine")
+	public Double cosine(@RequestParam Map<Double,Double> requestParameters) {
+		double numTerms = Double.valueOf(requestParameters.get("numTerms"));
+		double value = Double.valueOf(requestParameters.get("value"));
+		TaylorSeries cos = new Cosine();
+		double computedValue = cos.computeSeries(value, numTerms);
+		return computedValue;
+	}
 }
