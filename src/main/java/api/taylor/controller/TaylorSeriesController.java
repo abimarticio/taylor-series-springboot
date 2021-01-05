9761a7a2023c058719565564e1812be27c4665eb
Abigail Marticio <marticiobgl@gmail.com>
2021-01-05 23:44:01 +0800

    chore: add controller for taylor sine

--- src/main/java/api/taylor/controller/TaylorSeriesController.java
+++ src/main/java/api/taylor/controller/TaylorSeriesController.java
@@ -19,4 +19,13 @@ public class TaylorSeriesController {
 		double computedValue = cos.computeSeries(value, numTerms);
 		return computedValue;
 	}
+	
+	@GetMapping("/taylor-series/sine")
+	public Double sine(@RequestParam Map<Double,Double> requestParameters) {
+		double numTerms = Double.valueOf(requestParameters.get("numTerms"));
+		double value = Double.valueOf(requestParameters.get("value"));
+		TaylorSeries sin = new Sine();
+		double computedValue = sin.computeSeries(value, numTerms);
+		return computedValue;
+	}
 }
