4384ab470debc5d5111993618dcda43e88f896e1
Abigail Marticio <marticiobgl@gmail.com>
2021-01-11 23:05:51 +0800

    chore: add import of Cosine, Sine and Exponential

--- src/main/java/api/taylor/controller/TaylorSeriesController.java
+++ src/main/java/api/taylor/controller/TaylorSeriesController.java
@@ -7,6 +7,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import api.taylor.model.TaylorSeries;
+import api.taylor.model.Cosine;
+import api.taylor.model.Exponential;
+import api.taylor.model.Sine;
 
 
 @RestController
