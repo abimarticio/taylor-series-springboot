299a82b44acf240c6b0f309112fa552d40108253
Abigail Marticio <marticiobgl@gmail.com>
2021-01-11 22:56:04 +0800

    chore: remove child class Cosine

--- src/main/java/api/taylor/model/TaylorSeries.java
+++ src/main/java/api/taylor/model/TaylorSeries.java
@@ -13,21 +13,6 @@ public class TaylorSeries {
     }
 }
 
-class Cosine extends TaylorSeries {
-	
-    static double computeSeries(double x, double numTerms) {
-        x  = Math.toRadians(x);
-        double approximation = 0;
-        for (int i = 0; i < numTerms; i++) {
-            double coefficient = Math.pow((-1), i);
-            double numerator = Math.pow(x, (2 * i));
-            double denominator = factorial(2 * i);
-            approximation += (coefficient * (numerator / denominator));
-        }
-        return approximation;
-    } 
-}
-
 class Exponential extends TaylorSeries {
 	
     static double computeSeries(double x, double numTerms) {
