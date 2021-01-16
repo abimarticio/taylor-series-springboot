82763d8cca25a5160fb12d54e5ce75d68cf7fa7c
Abigail Marticio <marticiobgl@gmail.com>
2021-01-16 21:53:23 +0800

    fix: exchange exponential computeSeries with sine

--- src/main/java/api/taylor/model/Exponential.java
+++ src/main/java/api/taylor/model/Exponential.java
@@ -3,15 +3,13 @@ import java.lang.Math;
 
 
 public class Exponential extends TaylorSeries {
-	 public double computeSeries(double x, double numTerms) {
-	        x = Math.toRadians(x);
-	        double approximation = 0;
-	        for (int i = 0; i < numTerms; i++) {
-	            double coefficient = Math.pow((-1), i);
-	            double numerator = Math.pow(x, (2 * i + 1));
-	            double denominator = factorial(2 * i + 1);
-	            approximation += (coefficient * (numerator / denominator));
-	        }
-	        return approximation;
-	    }
+	public double computeSeries(double x, double numTerms) {
+        double approximation = 0;
+        for (int i = 0; i < numTerms; i++) {
+            double numerator = Math.pow(x, i);
+            double denominator = factorial(i);
+            approximation += (numerator / denominator);
+        }
+        return approximation;
+    }
 }
